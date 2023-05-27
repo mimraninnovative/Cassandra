df91d875c1a10d0ae9086623a20a3e96e15da185
DESKTOP-SQE5D8C\m.imran <m.imran@techlogix.com>
2023-05-27 19:57:55 +0500

    commit

--- CassandraConsoleApplication/Program.cs
+++ CassandraConsoleApplication/Program.cs
@@ -8,19 +8,14 @@ var cluster = Cluster.Builder()
 var session = cluster.Connect("techframer");
 
 var resultSet  = session.Execute("SELECT * FROM student ");
-var resultSetunique_identifiers_v2 = session.Execute("SELECT * from unique_identifiers_v2 ");
+ 
 foreach (var row in resultSet)
 {
     // Process each row of the result
     var columnValue = row.GetValue<string>("student_name");
     Console.WriteLine(columnValue);
 }
-foreach (var row in resultSetunique_identifiers_v2)
-{
-    // Process each row of the result
-    var columnValue = row.GetValue<string>("unique_identifier");
-    Console.WriteLine(columnValue);
-}
+ 
 session.Dispose();
 cluster.Dispose();
 
